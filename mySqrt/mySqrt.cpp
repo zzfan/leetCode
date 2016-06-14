34cfcaced08d917ae709dff5874be6c3963869ae
zzfan <zzfan@mail.ustc.edu.cn>
2016-06-14 14:53:22 +0800

    6.14

--- mySqrt/mySqrt.cpp
+++ mySqrt/mySqrt.cpp
@@ -10,6 +10,27 @@ using namespace std;
 
 int mySqrt(int x)
 {
+    if(x < 0) return -1;
+    if(x == 0) return 0;
+    int low = 1, high = x/2+1;
+    int mid;
+    while(low <= high){
+        mid = low + (high-low)/2;
+        if(x/mid >= mid && (mid+1) > x/(mid+1)){
+            return mid;
+        }else if(x/mid > mid){
+            low = mid+1;
+        }else{
+            high = mid-1;
+        }
+    }
+}
 
+int main()
+{
+    int n;
+    while(cin >> n){
+        cout << mySqrt(n) << endl;
+    }
 }
 
